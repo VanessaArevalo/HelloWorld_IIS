99c13cae2484f49eac42755c0da0d2938ebd6c17
VanessaArevalo <ae25016@ues.edu.sv>
2025-05-08 13:22:17 -0600

    Add files via upload

---
+++ src/main/java/y/App.java
@@ -0,0 +1,15 @@
+package y;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+	public String Hello () {
+		return "Hello World AE25016";
+	}
+    public static void main(String[] args) {
+    	App hw = new App() ;
+        System.out.println(hw.Hello() );
+    }
+}
---
+++ src/test/java/y/AppTest.java
@@ -0,0 +1,22 @@
+package y;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHelloAE25016() {
+        assertTrue ( true );
+    }
+
+	private void assertTrue (boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+}
